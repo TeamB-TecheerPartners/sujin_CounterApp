6c4e02008db065054af97bcf7d2c6f0627132700
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-15 01:17:25 +0900

    feat : 초기세팅 for Counter App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
+
+function App() {
+  return (
+    <>
+      <Viewer />
+    </>
+  );
+}
+
+export default App;
